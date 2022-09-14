fc90f00891b159ea513c927848016403302500ee
Cody <dakotalrubin@gmail.com>
2022-09-14 10:36:14 -0700

    Add my solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    // Return "ERROR" for non-numeric or negative arguments.
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) ||
+        num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    // If larger number first, create temp variable to swap argument order.
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    for (num1; num1 <= num2; num1++) {
+        sum += num1;
+    }
+
+    return sum;
 };
 
-// Do not edit below this line
 module.exports = sumAll;
