ea61389ca251ac31b965c271d3c280d08b1a9b5a
Cody <dakotalrubin@gmail.com>
2022-09-14 09:59:17 -0700

    Add my solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    // Counter variable i will loop through array elements.
+    for (let i = 0; i < array.length; i++) {
+        // Counter variable j will loop through args elements.
+        for (let j = 0; j < args.length; j++) {
+            if (array[i] === args[j]) {
+                array.splice(i, 1);
+                i--; // Reset array index to check the new value placed there.
+                break; // We don't check more args elements after removal.
+            }
+        }
+    }
+    return array;
 };
 
-// Do not edit below this line
 module.exports = removeFromArray;
