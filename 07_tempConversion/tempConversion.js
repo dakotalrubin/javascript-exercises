06433151044f0f63b8faa88d8e7b8fa7bd33204a
Cody <dakotalrubin@gmail.com>
2022-09-14 11:54:10 -0700

    Add my solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+    const temp = (num - 32) / 1.8;
+    return Math.round(temp * 10) / 10; // Round the result to one decimal place.
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+    const temp = (num * 1.8) + 32;
+    return Math.round(temp * 10) / 10; // Round the result to one decimal place.
 };
 
-// Do not edit below this line
 module.exports = {
   ftoc,
   ctof
