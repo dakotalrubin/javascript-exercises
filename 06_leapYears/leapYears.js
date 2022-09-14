c0eb4dbc46c2fc635259c25be22db77bd91f66d7
Cody <dakotalrubin@gmail.com>
2022-09-14 11:24:08 -0700

    Add my solution for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0 && year % 100 != 0) {
+    // Includes all leap years that don't involve multiples of 100
+        return true;
+    } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+        // Includes all leap years involving multiples of 100 that still count
+        return true;
+    } else {
+        // Excludes all other years
+        return false;
+    }
 };
 
-// Do not edit below this line
 module.exports = leapYears;
